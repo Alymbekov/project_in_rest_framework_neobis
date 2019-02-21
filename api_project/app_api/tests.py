db70921e095b66831f77acee85df8813fd753fef
Alymbekov <maxim.makarov.1997@mail.ru>
2019-02-22 00:04:14 +0600

    add test get request by id

--- api_project/app_api/tests.py
+++ api_project/app_api/tests.py
@@ -2,8 +2,9 @@ from django.urls import reverse
 from .models import *
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import  APITestCase
+from rest_framework.test import  APITestCase,RequestsClient
 import unittest
+
 # loader = unittest.TestLoader()
 # start_dir = 'project_in_rest_framework_neobis/api_project/apps/app_api/tests.py'
 # suite = loader.discover(start_dir)
@@ -12,6 +13,8 @@ import unittest
 # runner.run(suite)
 
 class Test_Get_Course(APITestCase):
+
+
     def test_get_course(self):
         """
         Проверяем get запрос на корректность
@@ -20,4 +23,18 @@ class Test_Get_Course(APITestCase):
         url = reverse('courses')
         response = self.client.get(url)
 
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+class Test_Get_Course_By_Id(APITestCase):
+
+
+    def test_get_course_by_id(self):
+        """
+        Проверяем get запрос по id на корректность
+        """
+        client = RequestsClient()
+        # response = client.get('http://127.0.0.1:8000/courses/14')
+        response = self.client.get('/courses/14/')
+        print(response)
+        # assert response.status_code == HTTP_200_OK
         self.assertEqual(response.status_code,status.HTTP_200_OK)
