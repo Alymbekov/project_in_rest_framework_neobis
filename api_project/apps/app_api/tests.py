8f3f778a93f3ecc8aef12a91bc71112f0819f5e4
Alymbekov <maxim.makarov.1997@mail.ru>
2019-02-21 18:46:11 +0600

    add a test example with library request

--- api_project/apps/app_api/tests.py
+++ api_project/apps/app_api/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-#writetesting
-# Create your tests here.
+from rest_framework.test import RequestsClient
+
+client = RequestsClient()
+response = client.get('http://127.0.0.1:8000/courses/')
+assert response.status_code == 200
